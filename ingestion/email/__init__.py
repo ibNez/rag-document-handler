8e0d26b651e541a4ccc32b9f9991d8b5b898c068
Tony Philip <tonyphilip@gmail.com>
2025-08-16 19:46:13 -0400

    Implement email ingestion runner

--- ingestion/email/__init__.py
+++ ingestion/email/__init__.py
@@ -3,10 +3,12 @@
 from .connector import EmailConnector, IMAPConnector
 from .orchestrator import EmailOrchestrator
 from .email_manager import EmailManager
+from .ingest import run_email_ingestion
 
 __all__ = [
     "EmailConnector",
     "IMAPConnector",
     "EmailOrchestrator",
     "EmailManager",
+    "run_email_ingestion",
 ]
